0c53e5c0c99cab771bf261084934894ec5075b5b
Bright Ozuzu Uzosike <michizler.brandforyou@gmail.com>
2024-12-25 09:55:07 +0100

    added config file to gitignore

--- bookstore-mern-stack/backend/config.js
+++
@@ -1,3 +0,0 @@
-export const PORT = 8000;
-
-export const mongoDBurl = "mongodb+srv://brightuzosike:bright12345@bookstore-mern.scbud.mongodb.net/books-collection?retryWrites=true&w=majority&appName=BookStore-MERN";
