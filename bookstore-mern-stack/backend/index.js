0b093a099af47266859841d933aeb4dcb9623a82
Bright Ozuzu Uzosike <michizler.brandforyou@gmail.com>
2024-12-25 16:29:13 +0100

    Made backend cors compliant

--- bookstore-mern-stack/backend/index.js
+++ bookstore-mern-stack/backend/index.js
@@ -1,12 +1,20 @@
 import express, { request } from "express";
 import { PORT, mongoDBurl } from "./config.js";
 import mongoose from "mongoose";
-// import { Book } from "./bookModel.js";
 import router from "./routes/booksRoute.js";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
+
+app.use(
+    cors({
+        origin: "http://localhost:8000",
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        allowedHeaders: ["Content-Type"]
+    })
+);
 
 app.get("/", (request, response)=>{
     console.log(request);
