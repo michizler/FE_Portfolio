f2e3c5ba9dc9cbcbe7c34897ea4441d396b18092
Bright Ozuzu Uzosike <michizler.brandforyou@gmail.com>
2024-12-30 00:51:42 +0100

    Added neccessary import in main.jsx (notistack)

--- bookstore-mern-stack/frontend/src/main.jsx
+++ bookstore-mern-stack/frontend/src/main.jsx
@@ -1,14 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
+import { SnackbarProvider } from "notistack";
 
-const root = ReactDOM.createRoot(document.getElementById('root')); // Create a root
+const root = ReactDOM.createRoot(document.getElementById("root")); // Create a root
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <SnackbarProvider>
+        <App />
+      </SnackbarProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
