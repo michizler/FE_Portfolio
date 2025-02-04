340a9a40f812be8d38a3ed27b72783e285101cfa
Bright Ozuzu Uzosike <michizler.brandforyou@gmail.com>
2025-02-04 19:37:05 +0100

    Added database connection to project

--- moxiescents/backend/config.js
+++ moxiescents/backend/config.js
@@ -1,1 +1,6 @@
 export const PORT = 5000;
+
+export const mongoDBURL =
+  "mongodb+srv://brightuzosike:bright12345@moxieproducts.ru373.mongodb.net/?retryWrites=true&w=majority&appName=moxieProducts";
+
+
--- moxiescents/backend/index.js
+++ moxiescents/backend/index.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { PORT } from "./config.js";
+import { PORT, mongoDBURL } from "./config.js";
 import path from "path"; // Use ES module import
 import { fileURLToPath } from "url";
 import cors from "cors";
+import mongoose from "mongoose";
 
 const app = express();
 
@@ -21,16 +22,25 @@ app.get("/", (req, res) => {
 });
 
 app.get("*", (req, res) => {
-    const indexPath = path.join(buildPath, "index.html");
-  
-    res.sendFile(indexPath, (err) => {
-      if (err) {
-        console.error("Error sending the file:", err);
-        res.status(500).send("An error occurred while retrieving the page.");
-      }
-    });
-  });
+  const indexPath = path.join(buildPath, "index.html");
 
-app.listen(PORT, () => {
-  console.log(`App is listening on port ${PORT}`);
+  res.sendFile(indexPath, (err) => {
+    if (err) {
+      console.error("Error sending the file:", err);
+      res.status(500).send("An error occurred while retrieving the page.");
+    }
+  });
 });
+
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log("App connected to database");
+    app.listen(PORT, () => {
+      console.log(`App is listening on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Error connecting to database:", error.message);
+    process.exit(1); // Exit the process with an error code
+  });
