9ce5e7c53017f5749d3752acd9c31dfe394f2470
Bright Ozuzu Uzosike <michizler.brandforyou@gmail.com>
2025-02-07 23:50:34 +0100

    Finished backend requests for product page

--- moxiescents/backend/index.js
+++ moxiescents/backend/index.js
@@ -4,32 +4,125 @@ import path from "path"; // Use ES module import
 import { fileURLToPath } from "url";
 import cors from "cors";
 import mongoose from "mongoose";
+import { Product } from "./models/productModel.js";
 
 const app = express();
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const buildPath = path.join(__dirname, "../frontend/build");
-
-app.use(express.static(buildPath));
 
 app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   console.log(req);
   return res.status(234).send("MoxieScents");
 });
 
-app.get("*", (req, res) => {
-  const indexPath = path.join(buildPath, "index.html");
 
-  res.sendFile(indexPath, (err) => {
-    if (err) {
-      console.error("Error sending the file:", err);
-      res.status(500).send("An error occurred while retrieving the page.");
+// Route for Saving A New Product To Database
+app.post("/products", async (request, response) => {
+  try {
+    if (
+      !request.body.name ||
+      !request.body.price ||
+      !request.body.inStock ||
+      !request.body.image ||
+      !request.body.rating
+    ) {
+      return response.status(400).send({
+        message:
+          "Send all required fields: name, price, inStock, image, rating",
+      });
     }
-  });
+    const newProduct = {
+      name: request.body.name,
+      price: request.body.price,
+      inStock: request.body.inStock,
+      image: request.body.image,
+      rating: request.body.rating,
+    };
+
+    const product = await Product.create(newProduct);
+
+    return response.status(201).send(product);
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
+
+// Route for Get All Products From Database
+app.get("/api/products", async (request, response) => {
+  try {
+    const products = await Product.find({});
+
+    return response.status(200).json({
+      count: products.length,
+      data: products,
+    });
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
+
+// Route for Updating a Product
+app.put("/products/:id", async (request, response) => {
+  try {
+    if (
+      !request.body.name ||
+      !request.body.price ||
+      !request.body.inStock ||
+      !request.body.image ||
+      !request.body.rating
+    ) {
+      return response.status(400).send({
+        message:
+          "Send all required fields: name, price, inStock, image, rating",
+      });
+    }
+
+    const { id } = request.params;
+
+    const result = await Product.findByIdAndUpdate(id, request.body);
+
+    if (!result) {
+      return response.status(404).json({ message: "Product not found" });
+    }
+
+    return response
+      .status(200)
+      .send({ message: "Product details updated successfully" });
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
+
+// Route for Deleting a Product
+app.delete("/products/:id", async (request, response) => {
+  try {
+    const { id } = request.params;
+
+    const result = await Product.findByIdAndDelete(id);
+
+    if (!result) {
+      return response.status(404).json({ message: "Product not found" });
+    }
+
+    return response
+      .status(200)
+      .send({ message: "Product deleted successfully" });
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
+
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, "client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/dist", "index.html"));
 });
 
 mongoose
---
+++ moxiescents/backend/models/productModel.js
@@ -0,0 +1,32 @@
+import { timeStamp } from "console";
+import mongoose from "mongoose";
+
+// Product Schema
+const ProductSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    inStock: {
+      type: Boolean,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    rating: {
+      type: Number,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Product = mongoose.model("Product", ProductSchema);
