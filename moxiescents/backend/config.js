119379c5b80aca65523b67cf01db9eb8241dcb8d
Bright Uzosike <michizler.brandforyou@gmail.com>
2025-02-21 15:35:35 +0100

    Delete moxiescents/backend/config.js

--- moxiescents/backend/config.js
+++
@@ -1,6 +0,0 @@
-export const PORT = 5000;
-
-export const mongoDBURL =
-  "mongodb+srv://brightuzosike:bright12345@moxieproducts.ru373.mongodb.net/?retryWrites=true&w=majority&appName=moxieProducts";
-
-
